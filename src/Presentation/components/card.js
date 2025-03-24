de703c8e20f0fab4e7e16f12507bcec7ad4c89c2
SrbastianM <sebas907.l@gmail.com>
2025-03-23 23:28:56 -0500

    Fix: Fix one bug with the footer, it not stand in the bottom of the page.

--- src/Presentation/components/card.js
+++ src/Presentation/components/card.js
@@ -5,7 +5,7 @@ import Title from "../atoms/title";
 export default function Card() {
   const card = `
     <div class="bg-white">
-        <div class="mx-auto max-w-2x1 px-4 py-16 sm:py-24 lg:max-w-7xl lg:px-8">
+        <div class="mx-auto max-w-2x1 px-4 py-16 sm:py-4 lg:max-w-7xl lg:px-8">
             ${Title("Customers Also Purchase")}
             <div class="mt-6 grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8">
                 <div class="group relative"> 
--- src/pages/home.js
+++ src/pages/home.js
@@ -1,10 +1,19 @@
 import Carousel from "/src/Presentation/organism/carousel";
 import Card from "../Presentation/components/card";
 export default function Home() {
+  const testingTest =
+    "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.c";
   return `
     <div>
       ${Carousel()}
-      <h1> Home Page </h1>
+      <section class="dark:bg-dark bg-white py-[70px]">
+        <div class="mx-auto px-4 sm:container">
+          <div class="border-stroke dark:border-dark-3 border-b">
+            <h2 class="text-dark mb-2 text-2xl font-semibold">Welcome to JS Shop</h2>
+            <p class="text-body-color dark:text-dark-6 mb-6 text-sm font-medium">${testingTest}</p>
+          </div>
+        </div>
+      </section>
       ${Card()}
     </div>
 `;
